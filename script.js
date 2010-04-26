8a8c9e81dbd8edc13d781856236d28b165f21982
Paul Driver <frodwith@gmail.com>
2010-04-26 13:10:55 -0700

    refactor into class

--- script.js
+++ script.js
@@ -1,67 +1,175 @@
 /*global YUI */
 
-YUI({filter: 'raw'}).use(
-    'yui2-dragdrop', 
-    'yui2-datatable', 
-    'node',
-    'tabview',
-    'event', function (Y) {
-        var YAHOO = Y.YUI2, tickets, dt, fmt, status, users, i, open_tab, tabs;
+YUI({filter: 'raw'}).use('yui2-dragdrop', 'yui2-datatable', 'node', 'tabview', 'event', function (Y) {
+    var YAHOO = Y.YUI2;
 
-        function parse_date(str) {
-            try { 
-                return Date.parse(str);
-            }
-            catch(e) { 
-                return false;
-            }
-        }
+    var Helpdesk = {
+        status: {
+            'pending'      : 'Pending',
+            'acknowledged' : 'Acknowledged',
+            'waiting'      : 'Waiting On External',
+            'feedback'     : 'Feedback Requested',
+            'confirmed'    : 'Confirmed',
+            'resolved'     : 'Resolved'
+        },
 
-        function lookup(map) {
-            return function(key) {
-                return map[key];
+        columns: function () {
+            function lookup(map) {
+                return function(key) {
+                    return map[key];
+                };
             }
-        }
 
-        function set_text(fn) {
-            return function (cell, record, column, data) {
-                cell.appendChild(document.createTextNode(fn(data)));
+            function set_text(fn) {
+                return function (cell, record, column, data) {
+                    Y.one(cell).set('text', fn(data));
+                };
+            }
+
+            var self = this;
+            var fmt = {
+                user   : set_text(lookup(self.users)),
+                status : set_text(lookup(self.status)),
+                date   : 'date',
+                link   : function (cell, record, column, t) {
+                    var a = Y.Node.create('<a>'), data = record._oData;
+                    a.setAttribute('href', data.url);
+                    a.set('text', t);
+                    Y.on('click', function (e) {
+                        var tab = self.open_tab(data)
+                        self.tabview.selectChild(tab.get('index'));
+                        e.halt();
+                    }, a);
+                    Y.one(cell).append(a);
+                }
             };
-        }
 
-        function digits(n, digits) {
-            var str = n.toString();
-            var diff = digits - str.length;
-            for (var i = 0; i < diff; i += 1) {
-                str = '0' + str;
+            return [
+                {   key       : 'id',
+                    label     : '#',
+                    sortable  : true,
+                    formatter : fmt.link
+                },
+                {   key       : 'title',
+                    label     : 'Title',
+                    sortable  : true,
+                    formatter : fmt.link
+                },
+                {   key       : 'opened_by',
+                    label     : 'Opened By',
+                    sortable  : true,
+                    formatter : fmt.user
+                },
+                {   key       : 'opened_on',
+                    label     : 'Opened On',
+                    sortable  : true,
+                    formatter : fmt.date
+                },
+                {   key       : 'assigned_to',
+                    label     : 'Assigned To',
+                    sortable  : true,
+                    formatter : fmt.user
+                },
+                {   key       : 'status',
+                    label     : 'Status',
+                    sortable  : true,
+                    formatter : fmt.status
+                },
+                {   key       : 'last_reply',
+                    label     : 'Last Reply',
+                    sortable  : true,
+                    formatter : fmt.date
+                }
+            ];
+        },
+        render: function () {
+            this.tabview.render();
+        },
+        datasource: function (tickets) {
+            function parseDate(str) {
+                try { 
+                    return Date.parse(str);
+                }
+                catch(e) { 
+                    return false;
+                }
             }
-            return str;
-        }
 
-        var tabs = {};
-        open_tab = function (data) {
-            var id = data.id, tab = tabs[id];
+            var source = new YAHOO.util.DataSource(tickets);
+            source.responseType = YAHOO.util.DataSource.TYPE_JSARRAY;
+            source.responseSchema = { 
+                fields: [
+                    { key: 'id',          parser: 'number'  },
+                    { key: 'url',         parser: 'string'  },
+                    { key: 'title',       parser: 'string'  },
+                    { key: 'opened_by',   parser: 'string'  },
+                    { key: 'opened_on',   parser: parseDate },
+                    { key: 'assigned_to', parser: 'string'  },
+                    { key: 'status',      parser: 'string'  },
+                    { key: 'last_reply',  parser: parseDate }
+                ]
+            };
+            return source;
+        },
+        create: function (args) {
+            var self       = Y.Object(this);
+            self.tabs      = {};
+            self.users     = args.users;
+            self.datatable = new YAHOO.widget.DataTable(
+                document.createElement('div'),
+                self.columns(), 
+                self.datasource(args.tickets)
+            );
+
+            var node = Y.one(self.datatable.get('element'));
+            node.addClass('yui3-tab-panel');
+
+            self.tabview = new Y.TabView({
+                children: [ { label: 'Tickets', panelNode: node } ]
+            });
+
+            return self;
+        },
+
+        close_tab: function(id) {
+            var tab = this.tabs[id];
+            delete this.tabs[id];
+            tab.remove();
+        },
+
+        open_tab: function (data) {
+            var id = data.id, tab = this.tabs[id];
+
             if (tab) {
                 return tab;
             }
-            tab = tabs[id] = new Y.Tab(
-                {   content : "foobaz",
-                    label   : id.toString() 
-                }
-            );
+
+            tab = this.tabs[id] = new Y.Tab({
+                content : "foobaz",
+                label   : id.toString() 
+            });
+
+            var closer = Y.bind(this.close_tab, this, id);
             tab.after('render', function () {
-                var a = document.createElement('a');
-                a.appendChild(document.createTextNode(' x'));
-                Y.on('click', function () {
-                    tab.remove();
-                }, a);
+                var a = Y.Node.create('<a> x</a>');
+                Y.on('click', closer, a);
                 tab.get('boundingBox').one('a').append(a);
             });
-            tabs.add(tab);
-            return tab;
-        };
 
-        tickets = [
+            this.tabview.add(tab);
+            return tab;
+        }
+    };
+
+    var helpdesk = Helpdesk.create({
+        users: {
+            'pdriver'  : 'Paul Driver',
+            'dbell'    : 'Doug Bell',
+            'fldillon' : 'Frank Dillon',
+            'xtopher'  : 'Chris Palamera',
+            'vrby'     : 'Jamie Vrbsky'
+        },
+        tickets: [
             {   id          : '12049',
                 url         : 'http://the.real.url/no_for_real/12049',
                 title       : 'My dog has no nose',
@@ -102,106 +210,8 @@ YUI({filter: 'raw'}).use(
                     }
                 ]
             }
-        ];
+        ]
+    });
 
-        status = {
-            'pending'      : 'Pending',
-            'acknowledged' : 'Acknowledged',
-            'waiting'      : 'Waiting On External',
-            'feedback'     : 'Feedback Requested',
-            'confirmed'    : 'Confirmed',
-            'resolved'     : 'Resolved'
-        };
-
-        users = {
-            'pdriver'  : 'Paul Driver',
-            'dbell'    : 'Doug Bell',
-            'fldillon' : 'Frank Dillon',
-            'xtopher'  : 'Chris Palamera',
-            'vrby'     : 'Jamie Vrbsky'
-        };
-
-
-        fmt = {
-            user   : set_text(lookup(users)),
-            status : set_text(lookup(status)),
-            date   : 'date',
-            link   : function (cell, record, column, data) {
-                var a = document.createElement('a');
-                a.href = record._oData.url;
-                a.appendChild(document.createTextNode(data));
-                Y.on('click', function (e) {
-                    var tab = open_tab(record._oData);
-                    tab.get('parent').selectChild(tab);
-                    e.halt();
-                }, a);
-                cell.appendChild(a);
-            },
-        };
-
-        dt = {
-            columns: [
-                {   key       : 'id',
-                    label     : '#',
-                    formatter : fmt.link
-                },
-                {   key       : 'title',
-                    label     : 'Title',
-                    formatter : fmt.link
-                },
-                {   key       : 'opened_by',
-                    label     : 'Opened By',
-                    formatter : fmt.user
-                },
-                {   key       : 'opened_on',
-                    label     : 'Opened On',
-                    formatter : fmt.date
-                },
-                {   key       : 'assigned_to',
-                    label     : 'Assigned To',
-                    formatter : fmt.user
-                },
-                {   key       : 'status',
-                    label     : 'Status',
-                    formatter : fmt.status
-                },
-                {   key       : 'last_reply',
-                    label     : 'Last Reply',
-                    formatter : fmt.date
-                }
-            ],
-            source: new YAHOO.util.DataSource(tickets),
-            widget: null
-        };
-
-        for (i = 0; i < dt.columns.length; i += 1) {
-            dt.columns[i].sortable = true;
-        }
-
-        dt.source.responseType = YAHOO.util.DataSource.TYPE_JSARRAY;
-        dt.source.responseSchema = { 
-            fields: [
-                { key: 'id',          parser: 'number'   },
-                { key: 'url',         parser: 'string'   },
-                { key: 'title',       parser: 'string'   },
-                { key: 'opened_by',   parser: 'string'   },
-                { key: 'opened_on',   parser: parse_date },
-                { key: 'assigned_to', parser: 'string'   },
-                { key: 'status',      parser: 'string'   },
-                { key: 'last_reply',  parser: parse_date }
-            ]
-        };
-
-        Y.on('available', function () {
-            dt.node   = document.createElement('div');
-            dt.widget = new YAHOO.widget.DataTable(
-                dt.node, dt.columns, dt.source
-            );
-            dt.node   = Y.one(dt.node);
-            dt.node.addClass('yui3-tab-panel');
-            tabs      = new Y.TabView({
-                children: [ { label: 'Tickets', panelNode: dt.node } ]
-            });
-            tabs.render();
-        }, '#container');
+    Y.on('available', Y.bind(Helpdesk.render, helpdesk), '#container');
 });
